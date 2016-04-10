49f3b09e4e40374961144b10215f1df6a39b41c9
Ryan Berndt <rberndt@users.noreply.github.com>
2016-04-10 17:19:51 -0400

    Added suse file

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 using namespace std;
 
-main()
+//This is Ryan's first code developed in openSUSE
+int main()
 {
-cout << "Hello World!" << endl;
+  cout << "Hello World!" << endl;
 return 0;
 }
